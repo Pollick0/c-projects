f2bc4effc840752ae2858ee33c2c99c661d2d7c4
Pollick0 <salman.shakhawat@outlook.com>
2025-03-12 17:39:53 -0600

    added gtk

--- calculator/main.c
+++ calculator/main.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main() {
-    printf("Type a number: \n");
-    int myNum;
-    scanf("%d", &myNum);
-
-    
-    return 0;
+static void on_activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello, World!");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_widget_show(window);
 }
- 
+
+int main(int argc, char **argv) {
+    GtkApplication *app = gtk_application_new("com.example.HelloWorld", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return status;
+}
